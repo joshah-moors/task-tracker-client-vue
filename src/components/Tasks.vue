d0c0ee6aef5286558de1a903d4449611be9d65e0
Joshah <joshah.moors@utexas.edu>
2020-03-08 10:03:45 -0500

    Added Tasks component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <p>tasks</p>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Tasks from '../components/Tasks.vue';
 import Ping from '../components/Ping.vue';
 
 Vue.use(Router);
@@ -8,6 +9,11 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      name: 'Tasks',
+      component: Tasks,
+    },
     {
       path: '/ping',
       name: 'Ping',
